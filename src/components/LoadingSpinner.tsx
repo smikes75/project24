42ec688eb84cc2f028e16b0a88fcab9adab4c1f9
Stepan Mikes <mikes@datahelp.cz>
2025-03-04 19:45:33 +0100

    Smazání všech souborů

--- src/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center min-h-[50vh]">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-primary"></div>
-    </div>
-  );
-}
--- src/utils/date.ts
+++
@@ -1,8 +0,0 @@
-export const formatDate = (dateString: string, locale = 'en'): string => {
-  const date = new Date(dateString);
-  return new Intl.DateTimeFormat(locale, {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  }).format(date);
-};
